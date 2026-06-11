9ff82472c85b2db714bdaaa0d1e36992ed39f1ee
bhardwajccs <153237951+bhardwajccs@users.noreply.github.com>
2026-06-10 22:23:35 -0600

    Update Views.sql

--- Scripts/Views.sql
+++ Scripts/Views.sql
@@ -15,10 +15,10 @@ Select * from v_col_customer;
 
 -- View based on Multiple Tables
 Create or replace view view_multiple_tables AS
-Select  N.N_Name,COUNT(C.C_CUSTKEY) from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER C
-Inner Join SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.NATION N
-ON C.C_NATIONKEY = N.N_NATIONKEY
-Group by all;
+    Select  N.N_Name,COUNT(C.C_CUSTKEY) CUSTOMER_COUNT from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER C
+    Inner Join SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.NATION N
+    ON C.C_NATIONKEY = N.N_NATIONKEY
+    Group by all;
 
 -- Materilazied View
 -- Storage Cost in Memory So performance compared to Normal Viw as data in m/m,
@@ -27,7 +27,7 @@ Group by all;
 -- LIMITATION -- More than one Table can't be Referenced
 
 Create or replace materialized view matv_customer AS
-Select * exclude(c_address) from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER;
+Select C_CUSTKEY, C_NAME, C_NATIONKEY, C_PHONE, C_ACCTBAL, C_MKTSEGMENT, C_COMMENT from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER;
 
 select * from matv_customer;
 
@@ -49,10 +49,13 @@ Group by N.N_Name;
  SHOW VIEWS;
 
 -- View Deff is Exposed -- Query Profile Somone can see all Columns
-Select * from v_customer
+Select * from v_customer;
 
 -- Solution - SECURE VIEW
 Create or replace secure view sv_col_customer AS
 Select * exclude(c_address) from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER;
 
 Select * from sv_col_customer;
+
+SELECT GET_DDL('VIEW', 'sv_col_customer');
+
