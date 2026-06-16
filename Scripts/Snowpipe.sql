b43aaed20d1d1ab8aeda686065d1c9176242da7d
bhardwajccs <153237951+bhardwajccs@users.noreply.github.com>
2026-06-15 21:40:08 -0600

    Create Snowpipe.sql

---
+++ Scripts/Snowpipe.sql
@@ -0,0 +1,170 @@
+/*
+
+Snowpipe is Snowflake's continuous data ingestion service. It automates loading data from files into Snowflake tables as soon as they become available in cloud storage, eliminating the need for manual scheduling or large, periodic batch loads
+
+No Manual COPY INTO -- It's Real-Time Pipelines -- No Scheduler / CRON Jobs is needed.
+
+Files Arrives >>> Snowpipe is Triggered as it receives Notification from Bucket >>> Data is loaded.
+
+Snowpipe uses Serveless means you do not pay per-cluster-uptime. Instead, Snowpipe is billed directly per gigabyte (GB) of data ingested.
+
+
+    
+Azure Blob Storage                  -- Files Lands
+        │
+        ▼
+Azure Event Grid                    -- It sends a message to a Storage Queue. 
+        │
+        ▼
+Azure Storage Queue       
+        │
+        ▼
+Snowflake Notification Integration   -- Snowflake Listens to Storage Queue
+        │
+        ▼
+Snowpipe (AUTO_INGEST = TRUE)        -- Triggered
+        │
+        ▼
+Snowflake Target Table               -- Data Loaded
+
+
+
+-- Requirements
+
+AZURE
+    SA
+    TenantID     = '5df7bfe8-c66e-4465-9a6b-7636fd5c6dd8'
+    ContainerURL = 'azure://lalitsa.blob.core.windows.net/source/'
+    Azure Storage Queue URL = https://lalitsa.queue.core.windows.net/snowpipequeuenew
+    
+    
+
+
+SNOWFLAKE
+    Table
+    Storage Integration
+    File Format
+    Stage
+    COPY INTO
+
+
+*/
+
+
+-- SNOWFLAKE
+
+-- STEP 1: Make Storage Integration
+
+CREATE OR REPLACE STORAGE INTEGRATION azure_storage_integration
+    TYPE = EXTERNAL_STAGE
+    STORAGE_PROVIDER = AZURE
+    ENABLED = TRUE
+    AZURE_TENANT_ID = '5df7bfe8-c66e-4465-9a6b-7636fd5c6dd8'
+    STORAGE_ALLOWED_LOCATIONS = 
+    ('azure://lalitsa.blob.core.windows.net/source/')
+    ;
+
+-- Step 1.1  
+-- Give permissions
+-- SA > IAM > Add Role > Storage Blob data Contributor > Select Members > Add Multi-tenant App name > Assign permissions
+DESC Storage Integration azure_storage_integration;
+
+-- STEP 2: File FORMAT
+CREATE OR REPLACE FILE FORMAT CSV_FORMAT_NEW
+                                        Type = CSV
+                                        SKIP_HEADER = 1
+                                        FIELD_DELIMITER = ','
+                                        RECORD_DELIMITER = '\n'
+                                        FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+                                        ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE;
+
+
+-- STEP 3: External Stage -- Tells Snowflake where Files are Located
+
+CREATE OR REPLACE STAGE Azure_Ext_Stg
+    STORAGE_INTEGRATION = azure_storage_integration
+    URL = 'azure://lalitsa.blob.core.windows.net/source/'
+    FILE_FORMAT = CSV_FORMAT_NEW
+    ;
+
+-- Check Connection
+ LIST @Azure_Ext_Stg;
+
+
+ -- See Data from Stage
+ Select $1, $2, $3, metadata$filename from @Azure_Ext_Stg limit 5;
+
+
+ -- Snowflake Table
+ CREATE OR REPLACE TABLE Financials(
+                                            Segment varchar,
+                                            Country varchar,
+                                            Product varchar,
+                                            DiscountBand varchar,
+                                            UnitsSold varchar,
+                                            ManufacturingPrice varchar,
+                                            SalePrice varchar,
+                                            GrossSales varchar,
+                                            Discount varchar,
+                                            Sales varchar,
+                                            COGS varchar,
+                                            Profit varchar,
+                                            Date varchar,
+                                            MonthNumber varchar,
+                                            MonthName varchar,
+                                            Year varchar,
+                                            __PowerAppsId__ varchar,
+                                            Filename varchar          -- Metadata extra column we added
+                                            );
+
+
+
+remove @Azure_Ext_Stg;
+
+
+-- STEP 4: 
+-- Snowpipe needs NOTIFICATION INTEGRATION
+-- for Snowflake to read Azure Queue messages
+
+-- SET UP Event on Azure
+
+--1. Register Event Grid in Azure from Azure CLI
+            -- type =     az provider register --namespace Microsoft.EventGrid
+            -- type =     az provider show --namespace Microsoft.EventGrid --query "registrationState"   -- Enter
+            
+--2  Storage Account >>> Events >>> New >>> Create Queue 
+
+-- 3. Copy QueueURL -- From SA >>> Queue
+
+CREATE OR REPLACE NOTIFICATION INTEGRATION Azure_Notification_Int
+    ENABLED = TRUE
+    TYPE = QUEUE
+    NOTIFICATION_PROVIDER = AZURE_STORAGE_QUEUE
+    AZURE_TENANT_ID = '5df7bfe8-c66e-4465-9a6b-7636fd5c6dd8'
+    AZURE_STORAGE_QUEUE_PRIMARY_URI = 'https://lalitsa.queue.core.windows.net/snowpipequeuenew'
+    ;
+
+DESC INTEGRATION Azure_Notification_Int;
+-- Follow same steps like SI
+-- SA > IAM > Storage Queue Data Contributor > Add
+
+
+
+-- STEP 5: SNOWPIPE
+
+CREATE PIPE Azure_Snowpipe
+    AUTO_INGEST = TRUE
+    INTEGRATION = Azure_Notification_Int
+    AS
+    COPY INTO Financials FROM @Azure_Ext_Stg;
+
+-- Finally
+Select * from Financials;
+
+
+TRUNCATE TABLE Financials;
+
+-- Dump some files in Blob -- Wait minutes
+
+-- Azure Snowpipe Status
+SELECT SYSTEM$PIPE_STATUS('Azure_Snowpipe');
