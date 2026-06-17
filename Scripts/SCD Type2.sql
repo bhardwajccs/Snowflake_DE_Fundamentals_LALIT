8dfff713e70a9d6b67c63f2f3069c284c0ab0954
bhardwajccs <153237951+bhardwajccs@users.noreply.github.com>
2026-06-16 23:32:06 -0600

    Update SCD Type2.sql

--- Scripts/SCD Type2.sql
+++ Scripts/SCD Type2.sql
@@ -1,163 +1,249 @@
--- SCD Type1,2
--- w Tasks and Streams
-
-CREATE OR REPLACE FILE FORMAT CSVONE
-TYPE = CSV  -- Default Value
-SKIP_HEADER = 1
-FIELD_DELIMITER = ','
-RECORD_DELIMITER = '\n'
-FIELD_OPTIONALLY_ENCLOSED_BY = '"';
-
-ALTER FILE FORMAT CSVONE
-SET date_format = 'yyyy-mm-dd';
+-- SCD Type 1 and TYPE 2
 
 
-CREATE OR REPLACE STORAGE INTEGRATION Azure_SI
-TYPE = external_stage
-STORAGE_PROVIDER = azure
-ENABLED = TRUE
-AZURE_TENANT_ID = '5df7bfe8-c66e-4465-9a6b-7636fd5c6dd8'   -- Copy from Azure Home Account
-STORAGE_ALLOWED_LOCATIONS = ('azure://extstgacc.blob.core.windows.net/filedata/');
+-- STEP 1: Make Storage Integration
+
+CREATE OR REPLACE STORAGE INTEGRATION azure_storage_integration
+    TYPE = EXTERNAL_STAGE
+    STORAGE_PROVIDER = AZURE
+    ENABLED = TRUE
+    AZURE_TENANT_ID = '5df7bfe8-c66e-4465-9a6b-7636fd5c6dd8'
+    STORAGE_ALLOWED_LOCATIONS = 
+    ('azure://lalitsa.blob.core.windows.net/source/')
+    ;
+
+-- Step 1.1  
+-- Give permissions
+-- SA > IAM > Add Role > Storage Blob data Contributor > Select Members > Add Multi-tenant App name > Assign permissions
+DESC Storage Integration azure_storage_integration;
+
+-- STEP 2: File FORMAT
+CREATE OR REPLACE FILE FORMAT CSV_FORMAT_NEW
+                                        Type = CSV
+                                        SKIP_HEADER = 1
+                                        FIELD_DELIMITER = ','
+                                        RECORD_DELIMITER = '\n'
+                                        FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+                                        ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE;
 
 
-DESC Integration Azure_SI;
+-- STEP 3: External Stage -- Tells Snowflake where Files are Located
 
-CREATE OR REPLACE STAGE Stage_Ext
-URL = 'azure://extstgacc.blob.core.windows.net/filedata/' -- Container URL -- Replace https w azure in URL
-FILE_FORMAT = CSVONE
-STORAGE_INTEGRATION = Azure_SI;
+CREATE OR REPLACE STAGE Azure_Ext_Stg
+    STORAGE_INTEGRATION = azure_storage_integration
+    URL = 'azure://lalitsa.blob.core.windows.net/source/'
+    FILE_FORMAT = CSV_FORMAT_NEW
+    ;
 
-SHOW Stages;
-
-LIST @Stage_Ext;
-
-CREATE OR REPLACE TABLE STG_Courses
-    (
-        CName Varchar,
-        Trainer Varchar
-    );
+-- Check Connection
+ LIST @Azure_Ext_Stg;
 
 
--- TYPE1
+ -- See Data from Stage
+ Select $1, $2, $3, metadata$filename from @Azure_Ext_Stg limit 5;
 
-CREATE OR REPLACE TABLE Courses_Type1
-    (
-        CName Varchar,
-        Trainer Varchar
-    );
 
--- TYPE2 Data
-CREATE OR REPLACE TABLE Courses_Type2
-    (
-        CName Varchar,
-        Trainer Varchar,
-        Start_Date date,
-        End_date Date DEFAULT date '2000-10-23',
-        Is_Active Boolean
-    );
+ -- Stage Table
+ CREATE OR REPLACE TABLE Financials_STG(
+                                            Segment varchar,
+                                            Country varchar,
+                                            Product varchar,
+                                            DiscountBand varchar,
+                                            UnitsSold varchar,
+                                            ManufacturingPrice varchar,
+                                            SalePrice varchar,
+                                            GrossSales varchar,
+                                            Discount varchar,
+                                            Sales varchar,
+                                            COGS varchar,
+                                            Profit varchar,
+                                            Date varchar,
+                                            MonthNumber varchar,
+                                            MonthName varchar,
+                                            Year varchar,
+                                            __PowerAppsId__ varchar,
+                                            Filename varchar          -- Metadata extra column we added
+                                            );
 
-Select current_date();
 
--- TASK1 -- AUTOMATE Truncation every 1 Minute
-CREATE OR REPLACE TASK Clean_Stage_Table
-Warehouse = 'COMPUTE_WH'
-SCHEDULE = '1 minute'
+-- Silver Layer Table
+CREATE OR REPLACE TABLE Financials(
+                                            Segment varchar,
+                                            Country varchar,
+                                            Product varchar,
+                                            DiscountBand varchar,
+                                            UnitsSold varchar,
+                                            ManufacturingPrice varchar,
+                                            SalePrice varchar,
+                                            GrossSales varchar,
+                                            Discount varchar,
+                                            Sales varchar,
+                                            COGS varchar,
+                                            Profit varchar,
+                                            Date varchar,
+                                            MonthNumber varchar,
+                                            MonthName varchar,
+                                            Year varchar,
+                                            __PowerAppsId__ varchar,
+                                            Filename varchar          -- Metadata extra column we added
+                                            );
+
+
+-- dimension
+-- Silver Layer Table
+CREATE OR REPLACE TABLE dimFinancials(
+                                            Segment varchar,
+                                            Country varchar,
+                                            Product varchar,
+                                            DiscountBand varchar,
+                                            UnitsSold varchar,
+                                            ManufacturingPrice varchar,
+                                            SalePrice varchar,
+                                            GrossSales varchar,
+                                            Discount varchar,
+                                            Sales varchar,
+                                            COGS varchar,
+                                            Profit varchar,
+                                            Date varchar,
+                                            MonthNumber varchar,
+                                            MonthName varchar,
+                                            Year varchar,
+                                            __PowerAppsId__ varchar,
+                                            Filename varchar,          -- Metadata extra column we added,
+                                            StatusFlag boolean,
+                                            StartDate date,
+                                            EndDate date
+                                            );
+
+
+-- Do AUTO.
+-- As SFNOWFLAKE doesn't allow Loading same file againa and again -- OR Forcefully we can Load but we can TRUNCATE w Task.
+CREATE OR REPLACE TASK CLEAN_STAGE_TABLE
+    WAREHOUSE = COMPUTE_WH
+    SCHEDULE = '1 MINUTE'
 AS
-TRUNCATE TABLE STG_COURSES;
+    TRUNCATE TABLE Financials_STG;
 
--- Task2
--- This is Child task RUNS After Parent task 1
--- LOAD Stage Table from Blob File
 
+-- Load Stage data
 CREATE OR REPLACE TASK LOAD_STAGE_DATA
-WAREHOUSE = COMPUTE_WH
-AFTER Clean_Stage_Table
+    WAREHOUSE = COMPUTE_WH
+    AFTER CLEAN_STAGE_TABLE
 AS
-COPY INTO STG_COURSES
-   FROM @Stage_ExtL;
+    COPY INTO Financials_STG FROM @Azure_Ext_Stg 
+        FILE_FORMAT = CSV_FORMAT_NEW;
 
--- Child task 2
--- Insert Data Into Silver Layer -- TYPE1
 
-Select * from COURSES_TYPE1;
-
-CREATE OR REPLACE TASK Courses_Typ1_LOAD
-WAREHOUSE = COMPUTE_WH
-AFTER LOAD_STAGE_DATA
+-- Silver Layer -- TYPE1 SCD
+CREATE OR REPLACE TASK Financials_SCD1_Load
+    WAREHOUSE = COMPUTE_WH
+    AFTER LOAD_STAGE_DATA
 AS
-MERGE INTO COURSES_TYPE1 crc_1
-USING (Select * from STG_COURSES) src
-ON crc_1.CNAME = src.CNAME
-WHEN MATCHED AND (crc_1.Trainer != src.Trainer)
-THEN UPDATE SET crc_1.Trainer = src.Trainer
-WHEN NOT MATCHED
-THEN INSERT (crc_1.CNAME, crc_1.Trainer) VALUES (srs.CNAME,src.Trainer);
+MERGE INTO FINANCIALS fin
+USING (SELECT * FROM Financials_STG) src
+ON fin.segment = src.segment and fin.country = src.country
+WHEN MATCHED AND (fin.Mobile != src.Mobile)
+THEN 
+    UPDATE SET fin.Mobile = src.Mobile
+WHEN NOT MATCHED THEN
+    INSERT ALL BY NAME;      -- If same columns in both source and destination tables
+    -- INSERT(fin.segement, fin.country.....) VALUES (src.segement, src.country....)
 
--- TASKS are SUPSNEDED
+
+ALTER TASK CLEAN_STAGE_TABLE RESUME;
+ALTER TASK LOAD_STAGE_DATA RESUME;
+ALTER TASK Financials_SCD1_Load RESUME;
+
 SHOW TASKS;
 
--- ENABLE TASKS
--- 1st ENABLE Child Tasks then PARENT Task
-ALTER TASK Clean_Stage_Table RESUME;
-ALTER TASK LOAD_STAGE_DATA RESUME;
-ALTER TASK Courses_Typ1_LOAD RESUME;
+SELECT * FROM TABLE(INFORMATION_SCHEMA.TASK_HISTORY());
 
-DROP TASK Clean_Stage_Table;
-DROP TASK LOAD_STAGE_DATA;
-DROP TASK Courses_Typ1_LOAD;
+-- Upload File
 
-SELEct * from STG_COURSES;
-select * from COURSES_TYPE1;
-
--- Check is TASK Executed ?
-
-Select * from table(INFORMATION_SCHEMA.TASK_HISTORY());
-
--- Check Data
-SELEct * from STG_COURSES;
-select * from COURSES_TYPE1;
+Select * from FINANCIALS_STG;
+Select * from FINANCIALS;
 
 
--- TYPE 2
--- SUSPEND Mian TASK
-ALTER TASK Clean_Stage_Table SUSPEND;
+ALTER TASK CLEAN_STAGE_TABLE SUSPEND;
 
--- CREATE STREAM on Type_1 Table for capturing changed data values
+-- SCD TYPE 2
 
-CREATE OR REPLACE STREAM Str_CoursesType1 on Table COURSES_TYPE1;
+-- Create STREAM on Top of TYPE1 Table in SILVER LAYER -- so that we can capture the changes from Stream.
+-- Work from here ???
 
-Select * from Str_CoursesType1;
+CREATE OR REPLACE STREAM ON TABLE Financials_STM;
 
--- Final task
-CREATE OR REPLACE TASK Courses_Type2_LOAD
-WAREHOUSE = COMPUTE_WH
-AFTER Courses_Typ1_LOAD
-WHEN
-SYSTEM$stream_has_data('Str_CoursesType1')
+Select * from Financials_STM;
+
+-- Task
+CREATE OR REPLACE TASK dimFinancials_LOAD
+    WAREHOUSE = COMPUTE_WH
+    AFTER Financials_SCD1_Load       -- TRGIGGER Based task as it works only if STREAM has data
+    WHEN SYSTEM$STREAM_HAS_DATA('Financials_STM')
 AS
-MERGE INTO Courses_Type2 dest
-USING (Select * from STG_COURSES) chk
-ON dest.CNAME = chk.CNAME
-WHEN MATCHED AND (chk.metadata$action ='DELETE')
-THEN UPDATE 
-       SET End_Date = current_date(),
-       IS_ACTIVE = 'FALSE'
-WHEN NOT MATCHED AND (chk.metadata$action ='INSERT')
-THEN INSERT 
-        (dest.CNAME, dest.Trainer, dest.start_date, dest.is_active) VALUES (chk.CNAME,chk.Trainer, current_date(), 'TRUE');
+
+MERGE INTO dimFinancial dimF
+USING (
+    SELECT *
+    FROM Financials_STM
+) chk
+ON dimF.segment = chk.segment
+AND dimF.country = chk.country
+
+WHEN MATCHED
+     AND (chk.metadata$action = 'DELETE')
+THEN
+    UPDATE SET
+        dimF.EndDate = CURRENT_DATE,
+        dimF.StatusFlag = 'False'
+
+WHEN NOT MATCHED
+     AND (chk.metadata$action = 'INSERT')
+THEN
+    -- INSERT ALL BY NAME;
+    INSERT (
+        dimF.Segment,
+        dimF.Country,
+        dimF.Product,
+        dimF.Discount Band,
+        dimF.Units Sold,
+        dimF.Manufacturing Price,
+        dimF.Sale Price,
+        dimF.Gross Sales,
+        dimF.Discount,
+        dimF.Sales,
+        dimF.COGS,
+        dimF.Profit,
+        dimF.Date
+        dimF.Month Number
+        dimF.Month Name
+        dimF.Year
+        dimF.__PowerAppsId__
+        dimF.Mobile,
+        dimF.StartDate,
+        dimF.StatusFlag
+    )
+    VALUES (
+        chk.Segment,
+        chk.Country,
+        chk.Product,
+        chk.Discount Band,
+        chk.Units Sold,
+        chk.Manufacturing Price,
+        chk.Sale Price,
+        chk.Gross Sales,
+        chk.Discount,
+        chk.Sales,
+        chk.COGS,
+        chk.Profit,
+        chk.Date
+        chk.Month Number
+        chk.Month Name
+        chk.Year
+        chk.__PowerAppsId__
+        chk.Mobile,
+        CURRENT_DATE,
+        'True'
+    );
 
 
-
-Select * from Courses_Type2;
-
-
-ALTER TASK Clean_Stage_Table RESUME;
-ALTER TASK LOAD_STAGE_DATA RESUME;
-ALTER TASK Courses_Typ1_LOAD RESUME;
-ALTER TASK Courses_Type2_LOAD RESUME;
-
-
-
-Select * from Courses_Type2;
-
-Select * from table(INFORMATION_SCHEMA.TASK_HISTORY());
