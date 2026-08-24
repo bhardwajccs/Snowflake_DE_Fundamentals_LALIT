b48f8bda5711dac98673e9c3c8af5a92c4b31c9a
bhardwajccs <153237951+bhardwajccs@users.noreply.github.com>
2026-08-24 16:14:19 -0600

    Update 4. COPY INTO Stages.sql

--- Scripts/4. COPY INTO Stages.sql
+++ Scripts/4. COPY INTO Stages.sql
@@ -1,20 +1,30 @@
 SHOW STAGES;
 
--- Remove all files from @STG_CSV
-REMOVE @STG_CSV;
+List @Azure_Ext_Stg_New
 
-COPY INTO @STG_CSV FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER;
+-- Remove all files from @STG_CSV
+REMOVE @Azure_Ext_Stg_New;
+
+
+-- Move Data from SF Tables into Stages  -- COPY INTO -- Downloading
+
+
+SELECT * FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER;
+
+COPY INTO @Azure_Ext_Stg_New FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER;
 
 -- File names are system generated like data_0_0_0.csv, then 0_1_0 ...
-LIST @STG_CSV;
+LIST @Azure_Ext_Stg_New
 
 -- Custom relevant File name
 -- And loading all data into One File only.
-COPY INTO @STG_CSV/Customer.csv 
-FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER 
-SINGLE = TRUE              -- While Unloading data how many files want to create.
-MAX_FILE_SIZE = 17000000   -- DEFAULT = 16 MB   -- MAX = 5 GB    (In EXTERNAL Cloud like AWS)
-OVERWRITE = TRUE;          -- To Reload Data agaian in Same File
+COPY INTO @Azure_Ext_Stg_New/Customerdata.csv 
+    FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER 
+    SINGLE = TRUE              -- While Unloading data how many files want to create.
+    MAX_FILE_SIZE = 17000000   -- DEFAULT = 16 MB   -- MAX = 5 GB    (In EXTERNAL Cloud like AWS)
+    OVERWRITE = TRUE;          -- To Reload Data agaian in Same File
+
+LIST @Azure_Ext_Stg_New
 
 Select $1, $2 from @STG_CSV/Customer.csv (FILE_FORMAT => 'CSV_TYPE');
 
@@ -34,7 +44,9 @@ LIST @STG_CSV;
 -- When we want to COUNT Records in each File in Stage
 -- DETAILED_OUTPUT = TRUE
 
-COPY INTO @STG_CSV/CustomerCount.csv 
+
+
+COPY INTO @Azure_Ext_Stg_New/Customerdata_2.csv
     FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER 
     DETAILED_OUTPUT = TRUE
     HEADER = TRUE       -- To see Col names
@@ -43,9 +55,9 @@ COPY INTO @STG_CSV/CustomerCount.csv
 
 -- Data VALIDATION Before Unlaoding into STAGE
 
-COPY INTO @STG_CSV/CustomerValidation.csv 
+COPY INTO @Azure_Ext_Stg_New/CustomerValidation.csv 
     FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER 
-    VALIDATION_MODE = RETURN_ROWS;
+    VALIDATION_MODE = RETURN_ROWS;    -- check
 
 LIST @STG_CSV; -- 13 Files
 
